ad89f944448d0934841a71ceae67cd41b79a7971
Robert Svebeck <robert@svebeck.com>
2022-03-31 22:08:41 +0200

    Better game experience

--- microbit_seekers/microbit-seekers.py
+++ microbit_seekers/microbit-seekers.py
@@ -1,68 +1,102 @@
 def on_received_number(receivedNumber):
-    global signal
-    signal = radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH)
-    if signal < -90:
-        basic.show_leds("""
-            # # # # #
-                        # # # # #
-                        # # # # #
-                        # # # # #
-                        # # # # #
-        """)
-    elif signal < -77:
-        basic.show_leds("""
-            . . . . .
-                        # # # # #
-                        # # # # #
-                        # # # # #
-                        # # # # #
-        """)
-    elif signal < -72:
-        basic.show_leds("""
-            . . . . .
-                        . . . . .
-                        # # # # #
-                        # # # # #
-                        # # # # #
-        """)
-    elif signal < -68:
-        basic.show_leds("""
-            . . . . .
-                        . . . . .
-                        # # # # #
-                        # # # # #
-                        # # # # #
-        """)
-    elif signal < -62:
-        basic.show_leds("""
-            . . . . .
-                        . . . . .
-                        . . . . .
-                        # # # # #
-                        # # # # #
-        """)
-    elif signal < -45:
-        basic.show_leds("""
-            . . . . .
-                        . . . . .
-                        . . . . .
-                        . . . . .
-                        # # # # #
-        """)
-    else:
-        basic.show_leds("""
-            # . . . #
-                        . # . # .
-                        . . # . .
-                        . # . # .
-                        # . . . #
-        """)
+    global signal, secretNumber, questStarted, Sender1Reciever2
+    if questStarted == 1:
+        signal = radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH)
+        if signal < -90:
+            basic.show_leds("""
+                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
+            """)
+        elif signal < -77:
+            basic.show_leds("""
+                . . . . .
+                                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
+            """)
+        elif signal < -72:
+            basic.show_leds("""
+                . . . . .
+                                . . . . .
+                                # # # # #
+                                # # # # #
+                                # # # # #
+            """)
+        elif signal < -68:
+            basic.show_leds("""
+                . . . . .
+                                . . . . .
+                                # # # # #
+                                # # # # #
+                                # # # # #
+            """)
+        elif signal < -62:
+            basic.show_leds("""
+                . . . . .
+                                . . . . .
+                                . . . . .
+                                # # # # #
+                                # # # # #
+            """)
+        elif signal < -45:
+            basic.show_leds("""
+                . . . . .
+                                . . . . .
+                                . . . . .
+                                . . . . .
+                                # # # # #
+            """)
+        else:
+            basic.show_leds("""
+                # . . . #
+                                . # . # .
+                                . . # . .
+                                . # . # .
+                                # . . . #
+            """)
+            if Sender1Reciever2 == 1:
+                secretNumber = receivedNumber
+            basic.show_number(secretNumber)
+            basic.pause(5000)
+            questStarted = 0
+            Sender1Reciever2 = 0
+            secretNumber = 0
+            basic.clear_screen()
 radio.on_received_number(on_received_number)
 
+def on_button_pressed_a():
+    global questStarted, Sender1Reciever2
+    if questStarted == 0:
+        questStarted = 1
+        Sender1Reciever2 = 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global secretNumber, questStarted, Sender1Reciever2
+    if questStarted == 0:
+        secretNumber = randint(1, 9)
+        basic.pause(5000)
+        basic.show_number(secretNumber)
+        basic.clear_screen()
+        questStarted = 1
+        Sender1Reciever2 = 2
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 signal = 0
+secretNumber = 0
+questStarted = 0
+Sender1Reciever2 = 0
+Sender1Reciever2 = 0
+questStarted = 0
+secretNumber = 0
 radio.set_transmit_power(7)
 radio.set_group(1)
 
 def on_forever():
-    radio.send_number(0)
+    if questStarted == 1:
+        radio.send_number(secretNumber)
 basic.forever(on_forever)
--- microbit_seekers/microbit_seekers.js
+++ microbit_seekers/microbit_seekers.js
@@ -1,90 +1,110 @@
-radio.onReceivedNumber(function (receivedNumber) {
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    
     if (questStarted == 1) {
         signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
         if (signal < -90) {
             basic.showLeds(`
                 # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
+                                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
+            `)
         } else if (signal < -77) {
             basic.showLeds(`
                 . . . . .
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
+                                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
+            `)
         } else if (signal < -72) {
             basic.showLeds(`
                 . . . . .
-                . . . . .
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
+                                . . . . .
+                                # # # # #
+                                # # # # #
+                                # # # # #
+            `)
         } else if (signal < -68) {
             basic.showLeds(`
                 . . . . .
-                . . . . .
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
+                                . . . . .
+                                # # # # #
+                                # # # # #
+                                # # # # #
+            `)
         } else if (signal < -62) {
             basic.showLeds(`
                 . . . . .
-                . . . . .
-                . . . . .
-                # # # # #
-                # # # # #
-                `)
+                                . . . . .
+                                . . . . .
+                                # # # # #
+                                # # # # #
+            `)
         } else if (signal < -45) {
             basic.showLeds(`
                 . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                # # # # #
-                `)
+                                . . . . .
+                                . . . . .
+                                . . . . .
+                                # # # # #
+            `)
         } else {
             basic.showLeds(`
                 # . . . #
-                . # . # .
-                . . # . .
-                . # . # .
-                # . . . #
-                `)
-            secretNumber = receivedNumber
+                                . # . # .
+                                . . # . .
+                                . # . # .
+                                # . . . #
+            `)
+            if (Sender1Reciever2 == 1) {
+                secretNumber = receivedNumber
+            }
+            
             basic.showNumber(secretNumber)
             basic.pause(5000)
             questStarted = 0
+            Sender1Reciever2 = 0
+            secretNumber = 0
+            basic.clearScreen()
         }
+        
     }
+    
 })
-input.onButtonPressed(Button.A, function () {
-    questStarted = 1
-})
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     if (questStarted == 0) {
-        secretNumber = randint(0, 9)
-        questStarted = secretNumber
-        basic.pause(5000)
-        basic.showNumber(0)
-        basic.clearScreen()
+        questStarted = 1
+        Sender1Reciever2 = 1
     }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (questStarted == 0) {
+        secretNumber = randint(1, 9)
+        basic.pause(5000)
+        basic.showNumber(secretNumber)
+        basic.clearScreen()
+        questStarted = 1
+        Sender1Reciever2 = 2
+    }
+    
 })
 let signal = 0
 let secretNumber = 0
 let questStarted = 0
+let Sender1Reciever2 = 0
+Sender1Reciever2 = 0
 questStarted = 0
 secretNumber = 0
 radio.setTransmitPower(7)
 radio.setGroup(1)
-basic.forever(function () {
+basic.forever(function on_forever() {
     if (questStarted == 1) {
         radio.sendNumber(secretNumber)
     }
+    
 })
